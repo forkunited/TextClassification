8edfa95bc5d2fbb96a20eabd23356bc2ae79f2b0
Bill McDowell <forkunited@gmail.com>
2014-05-28 01:46:12 -0400

    ...

---
+++ src/textclass/data/annotation/TextClassDocumentCategory.java
@@ -0,0 +1,25 @@
+package textclass.data.annotation;
+
+public enum TextClassDocumentCategory {
+	// 20newsgroups
+	COMP_GRAPHICS, //comp.graphics
+	COMP_OS_MS_WINDOWS_MISC, //comp.os.ms-windows.misc
+	COMP_SYS_IBM_PC_HARDWARE, //comp.sys.ibm.pc.hardware
+	COMP_SYS_MAC_HARDWARE, //comp.sys.mac.hardware
+	COMP_WINDOWS_X, //comp.windows.x 
+	REC_AUTOS, //rec.autos
+	REC_MOTORCYCLES, //rec.motorcycles
+	REC_SPORT_BASEBALL, //rec.sport.baseball
+	REC_SPORT_HOCKEY, //rec.sport.hockey
+	SCI_CRYPT, //sci.crypt
+	SCI_ELECTRONICS, //sci.electronics
+	SCI_MED, //sci.med
+	SCI_SPACE, //sci.space
+	MISC_FORSALE, //misc.forsale
+	TALK_POLITICS_MISC, //talk.politics.misc
+	TALK_POLITICS_GUNS, //talk.politics.guns
+	TALK_POLITICS_MIDEAST, //talk.politics.mideast
+	TALK_RELIGION_MISC, //talk.religion.misc
+	ALT_ATHEISM, //alt.atheism
+	SOC_RELIGION_CHRISTIAN //soc.religion.christian	
+}
