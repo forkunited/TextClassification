71a0fffe686b68ee628773acc0247c9bedadf660
Bill McDowell <forkunited@gmail.com>
2014-05-28 01:45:11 -0400

    ...

--- src/textclass/data/annotation/TextClassDocumentDatum.java
+++ src/textclass/data/annotation/TextClassDocumentDatum.java
@@ -2,6 +2,7 @@ package textclass.data.annotation;
 
 import ark.data.DataTools;
 import ark.data.annotation.Datum;
+import ark.data.annotation.Datum.Tools.LabelMapping;
 import ark.data.annotation.nlp.TokenSpan;
 
 public class TextClassDocumentDatum<L> extends Datum<L> {
@@ -67,6 +68,112 @@ public class TextClassDocumentDatum<L> extends Datum<L> {
 			
 			
 		};
+		
+		tools.addLabelMapping(new LabelMapping<TextClassDocumentCategory>() {
+			@Override
+			public String toString() {
+				return "FirstLevel";
+			}
+			
+			@Override
+			public TextClassDocumentCategory map(TextClassDocumentCategory label) {
+				if (label == TextClassDocumentCategory.ALT_ATHEISM)
+					return TextClassDocumentCategory.ALT_ATHEISM;
+				else if (label == TextClassDocumentCategory.COMP_GRAPHICS)
+					return TextClassDocumentCategory.COMP_GRAPHICS;
+				else if (label == TextClassDocumentCategory.COMP_OS_MS_WINDOWS_MISC)
+					return TextClassDocumentCategory.COMP_GRAPHICS;
+				else if (label == TextClassDocumentCategory.COMP_SYS_IBM_PC_HARDWARE)
+					return TextClassDocumentCategory.COMP_GRAPHICS;
+				else if (label == TextClassDocumentCategory.COMP_SYS_MAC_HARDWARE)
+					return TextClassDocumentCategory.COMP_GRAPHICS;
+				else if (label == TextClassDocumentCategory.COMP_WINDOWS_X)
+					return TextClassDocumentCategory.COMP_GRAPHICS;
+				else if (label == TextClassDocumentCategory.MISC_FORSALE)
+					return TextClassDocumentCategory.MISC_FORSALE;
+				else if (label == TextClassDocumentCategory.REC_AUTOS)
+					return TextClassDocumentCategory.REC_AUTOS;
+				else if (label == TextClassDocumentCategory.REC_MOTORCYCLES)
+					return TextClassDocumentCategory.REC_AUTOS;
+				else if (label == TextClassDocumentCategory.REC_SPORT_BASEBALL)
+					return TextClassDocumentCategory.REC_AUTOS;
+				else if (label == TextClassDocumentCategory.REC_SPORT_HOCKEY)
+					return TextClassDocumentCategory.REC_AUTOS;
+				else if (label == TextClassDocumentCategory.SCI_CRYPT)
+					return TextClassDocumentCategory.SCI_CRYPT;
+				else if (label == TextClassDocumentCategory.SCI_ELECTRONICS)
+					return TextClassDocumentCategory.SCI_CRYPT;
+				else if (label == TextClassDocumentCategory.SCI_MED)
+					return TextClassDocumentCategory.SCI_CRYPT;
+				else if (label == TextClassDocumentCategory.SCI_SPACE)
+					return TextClassDocumentCategory.SCI_CRYPT;
+				else if (label == TextClassDocumentCategory.SOC_RELIGION_CHRISTIAN)
+					return TextClassDocumentCategory.SOC_RELIGION_CHRISTIAN;
+				else if (label == TextClassDocumentCategory.TALK_POLITICS_GUNS)
+					return TextClassDocumentCategory.TALK_POLITICS_GUNS;
+				else if (label == TextClassDocumentCategory.TALK_POLITICS_MIDEAST)
+					return TextClassDocumentCategory.TALK_POLITICS_GUNS;
+				else if (label == TextClassDocumentCategory.TALK_POLITICS_MISC)
+					return TextClassDocumentCategory.TALK_POLITICS_GUNS;
+				else if (label == TextClassDocumentCategory.TALK_RELIGION_MISC)
+					return TextClassDocumentCategory.TALK_POLITICS_GUNS;
+				else 
+					return null;
+			}
+		});
+		
+		tools.addLabelMapping(new LabelMapping<TextClassDocumentCategory>() {
+			@Override
+			public String toString() {
+				return "SecondLevel";
+			}
+			
+			@Override
+			public TextClassDocumentCategory map(TextClassDocumentCategory label) {
+				if (label == TextClassDocumentCategory.ALT_ATHEISM)
+					return TextClassDocumentCategory.ALT_ATHEISM;
+				else if (label == TextClassDocumentCategory.COMP_GRAPHICS)
+					return TextClassDocumentCategory.COMP_GRAPHICS;
+				else if (label == TextClassDocumentCategory.COMP_OS_MS_WINDOWS_MISC)
+					return TextClassDocumentCategory.COMP_OS_MS_WINDOWS_MISC;
+				else if (label == TextClassDocumentCategory.COMP_SYS_IBM_PC_HARDWARE)
+					return TextClassDocumentCategory.COMP_SYS_IBM_PC_HARDWARE;
+				else if (label == TextClassDocumentCategory.COMP_SYS_MAC_HARDWARE)
+					return TextClassDocumentCategory.COMP_SYS_IBM_PC_HARDWARE;
+				else if (label == TextClassDocumentCategory.COMP_WINDOWS_X)
+					return TextClassDocumentCategory.COMP_WINDOWS_X;
+				else if (label == TextClassDocumentCategory.MISC_FORSALE)
+					return TextClassDocumentCategory.MISC_FORSALE;
+				else if (label == TextClassDocumentCategory.REC_AUTOS)
+					return TextClassDocumentCategory.REC_AUTOS;
+				else if (label == TextClassDocumentCategory.REC_MOTORCYCLES)
+					return TextClassDocumentCategory.REC_MOTORCYCLES;
+				else if (label == TextClassDocumentCategory.REC_SPORT_BASEBALL)
+					return TextClassDocumentCategory.REC_SPORT_BASEBALL;
+				else if (label == TextClassDocumentCategory.REC_SPORT_HOCKEY)
+					return TextClassDocumentCategory.REC_SPORT_BASEBALL;
+				else if (label == TextClassDocumentCategory.SCI_CRYPT)
+					return TextClassDocumentCategory.SCI_CRYPT;
+				else if (label == TextClassDocumentCategory.SCI_ELECTRONICS)
+					return TextClassDocumentCategory.SCI_ELECTRONICS;
+				else if (label == TextClassDocumentCategory.SCI_MED)
+					return TextClassDocumentCategory.SCI_MED;
+				else if (label == TextClassDocumentCategory.SCI_SPACE)
+					return TextClassDocumentCategory.SCI_SPACE;
+				else if (label == TextClassDocumentCategory.SOC_RELIGION_CHRISTIAN)
+					return TextClassDocumentCategory.SOC_RELIGION_CHRISTIAN;
+				else if (label == TextClassDocumentCategory.TALK_POLITICS_GUNS)
+					return TextClassDocumentCategory.TALK_POLITICS_GUNS;
+				else if (label == TextClassDocumentCategory.TALK_POLITICS_MIDEAST)
+					return TextClassDocumentCategory.TALK_POLITICS_GUNS;
+				else if (label == TextClassDocumentCategory.TALK_POLITICS_MISC)
+					return TextClassDocumentCategory.TALK_POLITICS_GUNS;
+				else if (label == TextClassDocumentCategory.TALK_RELIGION_MISC)
+					return TextClassDocumentCategory.TALK_RELIGION_MISC;
+				else 
+					return null;
+			}
+		});
 		
 		return tools;
 	}
